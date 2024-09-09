1313de7a9db4f5f05d5671a683c23972f8bf5690
kvebers <kvebers@student.42heilbronn.de>
2024-09-09 11:42:11 +0200

    Tracking correct number of LLM calls, remove request storage

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-chrome.runtime.onStartup.addListener(function () {
-  chrome.storage.local.clear(function () {});
-});
-
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     const requestData = {
@@ -10,13 +6,15 @@ chrome.webRequest.onBeforeRequest.addListener(
       requestBody: details.requestBody ? details.requestBody.raw : null,
     };
     if (details.url.includes("localhost")) return;
+
     let tempStorage = {
       httpRequestsMade: 0,
       requestsToWebpages: 0,
       requestsToLLMs: 0,
       consumptionOfWebpages: 0,
       consumptionOfLLMs: 0,
-      requests: [],
+      previousWebsite: "",
+      lastTime: 0,
     };
 
     chrome.storage.local.get(
@@ -24,39 +22,36 @@ chrome.webRequest.onBeforeRequest.addListener(
       function (result) {
         const permamentStorage = result.permamentStorage || tempStorage;
         permamentStorage.httpRequestsMade++;
-        if (details.url.includes("chatgpt") || details.url.includes("gemini")) {
+        const currentTime = new Date().getTime();
+
+        if (details.url.includes("https://chatgpt.com/backend-api/memories")) {
           permamentStorage.requestsToLLMs++;
           permamentStorage.consumptionOfLLMs += 4.76 + Math.random() * 2 - 1;
+        } else if (
+          details.url.includes("https://gemini.google.com/_/BardChatUi")
+        ) {
+          permamentStorage.requestsToLLMs += 0.25;
+          permamentStorage.consumptionOfLLMs +=
+            4.76 / 4 + Math.random() * 0.5 - 0.25;
+          permamentStorage.lastTime = currentTime;
         } else {
           if (details.method === "GET") {
+            permamentStorage.previousWebsite = details.url;
             permamentStorage.requestsToWebpages++;
             permamentStorage.consumptionOfWebpages +=
               1.74 + Math.random() * 0.5;
           }
         }
-        tempStorage = permamentStorage;
-        fetch("http://localhost:3001/api/requests", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            httpRequestsMade: permamentStorage.httpRequestsMade,
-            requestsToWebpages: permamentStorage.requestsToWebpages,
-            requestsToLLMs: permamentStorage.requestsToLLMs,
-            consumptionOfWebpages: permamentStorage.consumptionOfWebpages,
-            consumptionOfLLMs: permamentStorage.consumptionOfLLMs,
-          }),
-        })
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error("Network response was not ok");
-            }
-            return response.json();
-          })
-          .catch((error) => {});
-        permamentStorage.requests.push(requestData);
-        chrome.storage.local.set({ permamentStorage: permamentStorage });
+
+        tempStorage.httpRequestsMade = permamentStorage.httpRequestsMade;
+        tempStorage.requestsToWebpages = permamentStorage.requestsToWebpages;
+        tempStorage.requestsToLLMs = permamentStorage.requestsToLLMs;
+        tempStorage.consumptionOfWebpages =
+          permamentStorage.consumptionOfWebpages;
+        tempStorage.consumptionOfLLMs = permamentStorage.consumptionOfLLMs;
+        tempStorage.previousWebsite = permamentStorage.previousWebsite;
+        tempStorage.lastTime = permamentStorage.lastTime;
+        chrome.storage.local.set({ permamentStorage: tempStorage });
       }
     );
   },
--- popup.js
+++ popup.js
@@ -1,20 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const requestList = document.getElementById("request-list");
-  // chrome.storage.local.get(
-  //   { permamentStorage: { requests: [] } },
-  //   function (result) {
-  //     requestList.innerHTML = "";
-
-  //     const requests = result.permamentStorage.requests || [];
-  //     requests.forEach((request) => {
-  //       const listItem = document.createElement("li");
-  //       listItem.textContent = `${request.method}: ${request.url}`;
-  //       requestList.appendChild(listItem);
-  //     });
-  //   }
-  // );
   chrome.storage.local.get({ permamentStorage: {} }, function (result) {
-    requestList.innerHTML = "";
+    //requestList.innerHTML = "";
     const permamentStorage = result.permamentStorage || {};
     for (const key in permamentStorage) {
       if (key === "requests") continue;
@@ -23,5 +10,30 @@ document.addEventListener("DOMContentLoaded", function () {
       listItem.textContent = `${key}: ${value}`;
       requestList.appendChild(listItem);
     }
+  });
+
+  const clearButton = document.getElementById("clearButton");
+  clearButton.addEventListener("click", function () {
+    chrome.storage.local.clear(function () {
+      chrome.storage.local.get({ permamentStorage: {} }, function (result) {
+        const permamentStorage = result.permamentStorage || {};
+        permamentStorage.httpRequestsMade = 0;
+        permamentStorage.requestsToWebpages = 0;
+        permamentStorage.requestsToLLMs = 0;
+        permamentStorage.consumptionOfWebpages = 0;
+        permamentStorage.consumptionOfLLMs = 0;
+        permamentStorage.previousWebsite = "";
+        chrome.storage.local.set({ permamentStorage: permamentStorage });
+        const requestList = document.getElementById("request-list");
+        requestList.innerHTML = "";
+        for (const key in permamentStorage) {
+          if (key === "requests") continue;
+          const value = permamentStorage[key];
+          const listItem = document.createElement("li");
+          listItem.textContent = `${key}: ${value}`;
+          requestList.appendChild(listItem);
+        }
+      });
+    });
   });
 });
